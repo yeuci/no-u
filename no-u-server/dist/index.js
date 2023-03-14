73ddbb44c48dd94b72cb1bf16f29c12fa7c73ce2
mfitz6022 <mfitz6022@gmail.com>
2023-03-13 21:21:59 -0400

    server initialized

---
+++ no-u-server/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 8080;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('NO-U');
+});
+app.listen(port, () => {
+    console.log(`Express server listening on  port: ${port}`);
+});
+//# sourceMappingURL=index.js.map
